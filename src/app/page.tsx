2fb00192ac8898de7dec0765474cbf081ddf9665
Melyguy <101142491+Melyguy@users.noreply.github.com>
2025-09-07 22:01:42 +0200

    done finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,31 @@
-import Image from "next/image";
+"use client";
+import AnimatedText from "./Components/AnimatedText";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-full h-full  items-center justify-center">
+      <section className="h-screen w-screen flex flex-col items-center ">
+      <h1 className="text-5xl font-bold text-foreground mt-32">Hello, World!</h1>
+      <AnimatedText>
+      <p className="text-xl w-1/2 mt-16">In programming, “Hello, World” is usually the very first program you write when learning a new programming language. All it does is display the words Hello, World! on the screen. It’s a tradition because it shows the basic structure of a program: how to run code, use output, and confirm everything works.</p>
+      </AnimatedText>
+      </section>
+      <section className="w-screen h-screen bg-background flex flex-col items-center">
+      <h1 className="text-5xl font-bold text-foreground mt-32">Aura</h1>
+      <AnimatedText>
+      <p className="text-xl w-1/2 mt-16">An aura is often described as an invisible "energy field" that surrounds living things—like a glowing bubble or light around a person. Some people believe it reflects emotions, health, or spiritual state.</p>
+      </AnimatedText>
+      
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <section className="w-screen h-screen bg-background flex flex-col items-center">
+      <h1 className="text-5xl font-bold text-foreground mt-32">Sigma</h1>
+      <AnimatedText>
+      <p className="text-xl w-1/2 mt-16">A sigma is a person, usually a man in social slang, who values independence and self-reliance over fitting into social hierarchies. Unlike “alphas,” sigmas don’t seek attention or leadership roles; they quietly pursue their goals and live by their own rules. They are often seen as mysterious, confident, and capable, thriving outside traditional social structures.</p>
+      </AnimatedText>
+      
+
+      </section>
     </div>
   );
 }
