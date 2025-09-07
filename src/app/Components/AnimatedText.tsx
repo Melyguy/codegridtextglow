54eeb0da9b9b53c13961d149255f22f29bd25728
Melyguy <101142491+Melyguy@users.noreply.github.com>
2025-09-07 20:18:33 +0200

    started

---
+++ src/app/Components/AnimatedText.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, {useRef} from "react";
+
+export default function AnimatedText(){
+    const containerRef = useRef(null);
+    
+    return(<div ref={containerRef}>
+
+    </div>
+    );
+}
